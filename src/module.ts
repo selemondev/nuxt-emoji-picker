6bf5e9df498571950543482770a9d90dd4a439c6
selemondev <selemondev19@gmail.com>
2026-03-05 13:15:51 -0700

    feat: add Nuxt version 4 compatibility

--- src/module.ts
+++ src/module.ts
@@ -5,8 +5,8 @@ export default defineNuxtModule<Record<string, never>>({
     name: 'nuxt-emoji-picker',
     configKey: 'emoji',
     compatibility: {
-      nuxt: '>=3.0.0',
-    },
+      nuxt: '>=4.1.0'
+    }
   },
 
   defaults: {},
